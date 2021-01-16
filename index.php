14de3ba344aaaf8989c95e7fd315d87fcaaa9061
AdrianaMargiotta <adrianamargiotta@protonmail.com>
2021-01-16 17:16:07 +0100

    exercise completed

--- index.php
+++ index.php
@@ -7,38 +7,54 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php
+    <div class="container">
+        <?php
+            
+            class Store {
+                public $name;
+                public $location;
+                public $products;
 
-        class Store {
-            public $name;
-            public $location;
-            public $products;
-
-            public function __construct($name, $location) {
-                $this -> name = $name;
-                $this -> location = $location;
+                public function __construct($name, $location) {
+                    $this -> name = $name;
+                    $this -> location = $location;
+                }
+                public function print_store() {
+                    echo "Il nome dello stabilimento è: " . $this -> name . "<br>" . "ed è situato a: " . $this -> location . "<br>" . "Produce i seguenti modelli" . "<br>";
+                    foreach ($this -> products as $item) {
+                        echo "Nome del modello: " . $item -> name . "<br>" . "peso: " . $item -> weight. "<br>" . "prezzo: " . $item -> price . "<br>";
+                    }
+                }
             }
-            public function print() {
-                echo "Name: ". $this -> name . "<br>" . "Location: ". $this -> location. "<br>"; //. "Produzione: ". $this -> products;
+
+            class Product {
+                public $name;
+                public $weight;
+                public $price;
+
+                public function __construct($name = 'no name', $weight = 'no weight', $price = 'no price') {
+                    $this -> name = $name;
+                    $this -> weight = $weight;
+                    $this -> price = $price;
+                }
             }
-        }
-        $magazzino1 = new Store ("Maserati", "Bologna");
-        $magazzino1 -> print();
 
-        $magazzino2 = new Store ("Porsche", "Stoccarda");
-        // $magazzino2 -> products = "Tedesca";
-        $magazzino2 -> print();
+            $magazzino1 = new Store ("Maserati", "Bologna");
+            $product1 = new Product ("Levante", "2200Kg", "78.970€");
+            $product2 = new Product ("Ghibli", "2000Kg", "74.150€");
+            $product3 = new Product ("Quattroporte", "2065Kg", "130.480€");
+            $magazzino1 -> products = [$product1, $product2, $product3];
 
-       
+            $magazzino2 = new Store ("Porsche", "Stoccarda");
+            $product1 = new Product ("911", "1765Kg", "109.879€");
+            $product2 = new Product ("Cayenne", "2430Kg", "80.623€");
+            $magazzino2 -> products = [$product1, $product2];
+        ?>
 
+        <p> <?php $magazzino1 -> print_store(); ?> </p>
 
-    ?>
+        <p> <?php $magazzino2 -> print_store(); ?> </p>
+    </div>
+   
 </body>
 </html>
-
-
-
-<!-- Definire la classe Magazzino e la classe Prodotto nel seguente modo:
-Magazzino: definire gli attributi per nome, location e prodotti contenuti (array); definire inoltre costruttore con solo nome e location obbligatori
-Prodotto: definire gli attributi per nome, peso e prezzo
-Generare alcune istanze di ogni classe, hai fini di test e per meglio comprendere i concetti -->
